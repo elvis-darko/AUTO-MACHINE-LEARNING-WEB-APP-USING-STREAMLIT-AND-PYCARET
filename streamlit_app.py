77c46dfaf7693ae76369faae92a43b1f8112b75c
Elvis DARKO <elvis_darko@outlook.com>
2023-10-22 17:19:48 +0000

    App UPDATE

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,8 @@ from streamlit_option_menu import option_menu
 import os
 import pycaret
 
-
+with st.sidebar:
+    st.image("https://www.shutterstock.com/image-photo/machine-learning-artificial-intelligence-concept-computer-548182375?utm_campaign=image&utm_medium=googleimages&utm_source=iptc")
+    st.title("AUTO-MACHINE LEARNING APP DEVELOPED BY ELVIS DARKO")
+    st.radio()
 st.write("Hello, world")
