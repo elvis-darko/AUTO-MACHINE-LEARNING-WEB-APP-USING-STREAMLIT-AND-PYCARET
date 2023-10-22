f7c9e9574a2097d5e546e226145069b1abb04752
Elvis DARKO <elvis_darko@outlook.com>
2023-10-22 17:54:15 +0000

    App UPDATE

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,33 @@ from streamlit_option_menu import option_menu
 import os
 import pycaret
 
-# set App Page
+# set App PageS and style
 st.title("AUTO-MACHINE LEARNING APP DEVELOPED BY ELVIS DARKO")
+
+css_style = {
+    "icon": {"color": "white"},
+    "nav-link": {"--hover-color": "grey"},
+    "nav-link-selected": {"background-color": "#FF4C1B"},
+}
+
 
 # inks to images
 auto_url = "https://github.com/elvis-darko/AUTO-MACHINE-LEARNING-WEB-APP-USING-STREAMLIT-AND-PYCARET/raw/main/images/AUTOML.jpg"
 
+
+def home_page():
+    st.title()
+
+def developers_page():
+     st.title('THE APP DEVELOPERS')
+     dev_url = "https://github.com/elvis-darko/Team_Zurich_Capstone_Project/raw/main/Assets/images/developer.png"
+     st.image(dev_url, caption='Team Zurich Churn Prediction App', use_column_width=True)
+     st.write(f"""
+    <p>The following individuals contributed to the development of this streamlit churn app:</p>
+    """, unsafe_allow_html=True)
+
+
+# Set up option menu (side bar)
 with st.sidebar:
     st.image(auto_url, use_column_width=True)
     selected = option_menu(
@@ -19,3 +40,11 @@ with st.sidebar:
         styles=css_style
    )
     
+if selected == "Home":
+    home_page()
+
+elif selected == "Prediction":
+    prediction_page()
+
+elif selected == "Developers":
+    developers_page()
