ec31d5017b1eb05d7a0f4036b5677712344b2af0
Elvis DARKO <elvis_darko@outlook.com>
2023-10-22 17:46:57 +0000

    App UPDATE

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,18 @@ from streamlit_option_menu import option_menu
 import os
 import pycaret
 
+# set App Page
+st.title("AUTO-MACHINE LEARNING APP DEVELOPED BY ELVIS DARKO")
+
+# inks to images
+auto_url = "https://github.com/elvis-darko/AUTO-MACHINE-LEARNING-WEB-APP-USING-STREAMLIT-AND-PYCARET/raw/main/images/AUTOML.jpg"
+
 with st.sidebar:
-    st.image("https://www.shutterstock.com/image-photo/machine-learning-artificial-intelligence-concept-computer-548182375?utm_campaign=image&utm_medium=googleimages&utm_source=iptc")
-    st.title("AUTO-MACHINE LEARNING APP DEVELOPED BY ELVIS DARKO")
-    st.radio()
-st.write("Hello, world")
+    st.image(auto_url, use_column_width=True)
+    selected = option_menu(
+        menu_title=None,
+        options=["Home", "Prediction", "Developers"],
+        icons=["house", "droplet", "people"],
+        styles=css_style
+   )
+    
